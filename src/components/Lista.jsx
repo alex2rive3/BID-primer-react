d5335e1eb1c103cd28712d262c6cba0ff989fd26
alex2rive3 <alex2rive3@gmail.com>
2023-01-09 18:54:04 -0300

    Primera pagina en React

---
+++ src/components/Lista.jsx
@@ -0,0 +1,12 @@
+const Lista = () => {
+    return <>
+    <h2>Things I need to do</h2>
+    <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+    </ul>
+    </>
+}
+export default Lista;
